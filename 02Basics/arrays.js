498c69608906d020fbc7eb09cfae56d8acb9450b
Hridoy Kazi <62054154+hridoykazii@users.noreply.github.com>
2026-08-31 11:37:23 +0000

    join trics

--- 02Basics/arrays.js
+++ 02Basics/arrays.js
@@ -26,9 +26,37 @@
 // console.log(cars.sort());
 
 // **** Accessing the last Array Element
+// const cars = ['saab', 'volvo', 'bmw'];
+// const lastArray = cars[cars.length - 1];
+// console.log(lastArray);
+
+// *** Adding Array Element
+
+// const carsClassic = ['saab', 'volvo', 'bmw'];
+// const newCars = cars.push('toyota');
+// console.log(cars);
+// const carsRacing = ['Audi','Ferrari', 'Ford'];
+// const cars = carsClassic.push(carsRacing);
+// console.log(carsClassic);
+
+// *** new Array
+
+// const points = new Array(40);
+// console.log(points);
+
+// const cars = ['saab', 'volvo', 'bmw'];
+// cars.length = 44; // array length set kora jaba 
+// console.log(cars.length);
+
+// Join Array
 const cars = ['saab', 'volvo', 'bmw'];
-const lastArray = cars[cars.length - 1];
-console.log(lastArray);
+const joinCars = cars.join('-');
+console.log(joinCars);
+console.log('typeof', typeof(joinCars));
+
+
+
+
 
 
 
