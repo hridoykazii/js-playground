d23aba072cf6fd2909ed83f26a5a0ff3545f8b42
Hridoy Kazi <62054154+hridoykazii@users.noreply.github.com>
2026-08-31 06:38:02 +0000

    arrays

---
+++ 02Basics/arrays.js
@@ -0,0 +1,38 @@
+// const cars = ['saab', 'volvo', 'bmw']
+// console.log(cars[0]);
+
+// const cars =[];
+// cars[0]='saab',
+// cars[1]='volvo',
+// cars[2]='bmw'
+// console.log(cars);
+
+// **** Changing Array Element
+// const cars = ['saab', 'volvo', 'bmw'];
+// cars[0]='toyota';
+// console.log(cars);
+
+// ***** Converting Array to String
+
+// const cars = ['saab', 'volvo', 'bmw'];
+// const carsArrayToString = cars.toString();
+// console.log(carsArrayToString);
+
+// *** Displaying An Array using json
+// const cars = ['saab', 'volvo', 'bmw'];
+// const arrayToJson = JSON.stringify(cars);
+// console.log(typeof(arrayToJson));
+// console.log(cars.length);
+// console.log(cars.sort());
+
+// **** Accessing the last Array Element
+const cars = ['saab', 'volvo', 'bmw'];
+const lastArray = cars[cars.length - 1];
+console.log(lastArray);
+
+
+
+
+
+
+
